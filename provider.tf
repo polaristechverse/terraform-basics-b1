b5fa6e3c99430abec57ceec097c3a982945edf33
chaitanya <chaitanya@gmail.com>
2025-11-22 07:42:52 +0530

    addded remote backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
-provider "aws" {
-
+provider "aws" {}
+terraform {
+  backend "s3" {
+    bucket         = "cgit-terraform-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "DemoS3" {
+  bucket = "cgit-terraform-backend"
+}
